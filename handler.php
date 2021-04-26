586d6d3fb47e2a49130e5f1340dcb1fcc9fffebd
RITIKINGLE <78408525+RITIKINGLE@users.noreply.github.com>
2021-04-26 12:06:54 +0530

    go

--- handler.php
+++ handler.php
@@ -21,6 +21,6 @@ $validator->field('message')->maxLength(6000);
 
 
 
-$pp->sendEmailTo('bullsimpacts@gmail.com'); // ← Your email here
+$pp->sendEmailTo('ritikingle.9689@gmail.com'); // ← Your email here
 
 echo $pp->process($_POST);
